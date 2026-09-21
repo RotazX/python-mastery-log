e6ffc83d69b5b996acf9fa5f6bc14f6b9dd1be47
Georg Entsik <entsikud@gmail.com>
2026-09-21 21:07:28 +0300

    Day 8: recursion, lambdas, higher-order functions

---
+++ week02/recursion_basics.py
@@ -0,0 +1,47 @@
+
+def factorial(n: int) -> tuple[int | None, str | None]:
+    if n < 0:
+        return None, ValueError("The number should be higher than 0.")
+    if n == 0: # Base case
+        return 1, None
+    elif n == 1: # Base case
+        return 1, None
+    else: # Recursive case
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result, None
+    
+
+def fibonacci(n: int) -> tuple[int | None, str | None]:
+    if n == 0: # Base case
+        return 0, None
+    if n == 1: # Base case
+        return 1, None
+    else: # Recursive case
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+         a, b = b, a + b
+        return b, None
+
+def main() -> int:
+    try:
+        num = input("Give me a number. ")
+        r_num = int(num)
+    except ValueError:
+        print(f"{num} is not a number.")
+
+    fac_ans, fac_err = factorial(r_num)
+    if fac_err:
+        print(f"Error = {fac_err}")
+    else:
+        print(f"The factorial of {num} is {fac_ans}.")
+
+    fib_ans, fib_err = fibonacci(r_num)
+    if fib_err:
+        print(f"Error = {fib_err}")
+    else:
+        print(f"The fibonacci of {num} is {fib_ans}.")
+
+if __name__ == "__main__":
+    main()
