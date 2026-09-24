b879fcc934222b2f9f4194bf1c1f0754d811e401
Georg Entsik <entsikud@gmail.com>
2026-09-24 08:46:16 +0300

    Day 10: Big O basics + Week 2 capstone

---
+++ week02/bubble_sort.py
@@ -0,0 +1,10 @@
+
+def bubble_sort(numbers: list[int]) -> list[int]:
+    n = len(numbers)
+    for i in range(n - 1):
+        swapped = False
+        
+
+
+if __name__ == "__main__":
+    print(bubble_sort([5, 2, 9, 1]))
