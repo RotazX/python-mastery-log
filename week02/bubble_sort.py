412f7fc56dbf062db1b17926bf8a09da338f5b13
Georg Entsik <entsikud@gmail.com>
2026-09-24 15:07:02 +0300

    Day 10: bubble sort working

--- week02/bubble_sort.py
+++ week02/bubble_sort.py
@@ -3,8 +3,24 @@ def bubble_sort(numbers: list[int]) -> list[int]:
     n = len(numbers)
     for i in range(n - 1):
         swapped = False
-        
+        for j in range(n - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+                swapped = True
+        if not swapped:
+            break
+    return numbers
+
+def sort(numbers: list[int]) -> list[int]:
+    return sorted(numbers, reverse=False)
 
 
 if __name__ == "__main__":
-    print(bubble_sort([5, 2, 9, 1]))
+    if bubble_sort([5, 2, 9, 1, 7, 3]) == sort([5, 2, 9, 1, 7, 3]):
+        print("True")
+    else:
+        print("False")
+    
+    print(bubble_sort([5, 2, 9, 1, 7, 3])) 
+    print(sort([5, 2, 9, 1, 7, 3]))
+    
